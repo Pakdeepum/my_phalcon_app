d3b3050db0f8a61ec832ca4e808d71fe54f35cca
Pakdeepum <pakdeepumpi@gmail.com>
2018-04-25 12:09:24 +0700

    test

--- app/controllers/EventController.php
+++ app/controllers/EventController.php
@@ -35,7 +35,22 @@ class EventController extends ControllerBase{
       $event->name=$name;
       $event->date=$date;
       $event->detail=$detail;
-      $event->picture=$picture;
+      $photoUpdate='';
+  if($this->request->hasFiles() == true){
+    $allowed = array("jpg" => "image/jpg", "jpeg" => "image/jpeg", "gif" => "image/gif", "png" => "image/png");
+    $uploads = $this->request->getUploadedFiles();
+    $isUploaded = false;			
+    foreach($uploads as $upload){
+      if(in_array($upload->gettype(), $allowed)){					
+        $photoName=md5(uniqid(rand(), true)).strtolower($upload->getname());
+        $path = '../public/img/'.$photoName;
+        ($upload->moveTo($path)) ? $isUploaded = true : $isUploaded = false;
+      }
+    }
+    if($isUploaded)  $photoUpdate=$photoName ;
+    }else
+    die('You must choose at least one file to send. Please try again.');
+      $event->picture=$photoUpdate;
       $event->save();
       $this->response->redirect('event');
       }
